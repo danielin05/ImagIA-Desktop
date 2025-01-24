820d3bbdaca7295c38c343ea972101c71b2cf552
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2025-01-24 16:20:36 +0100

    layout inputs y botones

--- desktop/lib/main.dart
+++ desktop/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,29 +21,53 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueAccent,
-          title: const Text('IMAGIA DESKTOP', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),),
+          title: const Text(
+            'IMAGIA DESKTOP',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
+          ),
           centerTitle: true,
         ),
-      body: const PaginaLogIn(),
-      )
+        body: const PaginaLogIn(),
+      ),
     );
   }
 }
 
-class PaginaLogIn extends StatelessWidget {
+class PaginaLogIn extends StatefulWidget {
   const PaginaLogIn({super.key});
+
+  @override
+  _PaginaLogInState createState() => _PaginaLogInState();
+}
+
+class _PaginaLogInState extends State<PaginaLogIn> {
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        const Center(
+        Center(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 400, vertical: 25),
+            padding: const EdgeInsets.symmetric(horizontal: 400, vertical: 25),
             child: TextField(
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                ),
                 hintText: 'URL Servidor',
               ),
             ),
@@ -53,7 +78,15 @@ class PaginaLogIn extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 400, vertical: 25),
             child: TextFormField(
               decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                ),
                 labelText: 'Usuario',
               ),
             ),
@@ -63,26 +96,56 @@ class PaginaLogIn extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 400, vertical: 25),
             child: TextFormField(
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
+              decoration: InputDecoration(
+                border: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                ),
                 labelText: 'Contraseña',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscureText ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                    color: Colors.blue,
+                  ),
+                  onPressed: _togglePasswordVisibility,
+                ),
               ),
-              obscureText: true,
+              obscureText: _obscureText,
+              onChanged: (value) {
+                Future.delayed(const Duration(seconds: 1), () {
+                  if (mounted) {
+                    setState(() {
+                      _obscureText = true;
+                    });
+                  }
+                });
+              },
             ),
           ),
         ),
         Center(
-          child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white, 
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-              textStyle: TextStyle(fontSize: 18),
-            ),
-            child: Text('Iniciar Sesión'),
+          child: IconButton(
+            iconSize: 60,
+            icon: const Icon(Icons.arrow_forward_outlined),
+            onPressed: () {
+              Fluttertoast.showToast(
+                msg: "Usuario guardado correctamente",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.blue,
+                textColor: Colors.black,
+                fontSize: 16.0
+            );
+            },
+            color: Colors.blue,
           ),
-        )
+        ),
       ],
     );
   }
